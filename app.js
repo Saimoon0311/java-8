77b894b85ab940f88134732d28114741c3524248
Saimoon0311 <shaheer031123@gmail.com>
2020-07-18 02:43:02 -0700

    java 8 assignment

---
+++ app.js
@@ -0,0 +1,129 @@
+// chatper no 38-44
+
+// task 4
+
+
+
+// function main(){
+//     var one = document.getElementById("one")
+//     var two = document.getElementById("two")
+//     var three = document.getElementById("three")
+//     var one = one.value
+//     var two = two.value
+//     var three = three.value   
+// }
+// function per(){
+//     var per1 = (one * 1000 /100)
+//     var per2 = (two * 1000 /100)
+//     var per3 = (three * 1000 /100)
+//     console.log (per1 + per2 +per3)
+// }
+// function aver(){
+//     var av = (one + two + three / 3)
+//     console.log (av)
+// }
+
+
+
+// chapter no 43 - 48
+
+// task no 1
+
+
+// function hy(){
+// alert ("hello friends")
+// }
+
+
+// task 2
+
+
+// function h(){
+//    alert("Thank you for purchanig our mobile")
+// }
+
+
+// task 3
+
+// function deleteRow(r) {
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+//   }
+
+
+// task 4
+
+// function imageChange(){
+//     var carimg = document.getElementById("car")
+//     carimg.src= "13.jpg"
+// }
+
+// function imageChangeBefore(){
+//     var carimg = document.getElementById("car")
+//     carimg.src= "14.jpg"
+// }
+
+
+// task 5
+
+// var i = 0
+
+
+// function up(){
+//     document.getElementById("uper").innerHTML = i+=1
+// }
+// function dow(){
+//     document.getElementById("uper").innerHTML = i-=1
+// }
+
+// chapter no 49-52
+
+// task 1
+
+// function myFunction() {
+//     document.getElementById("myForm").submit();
+//   }
+
+
+// task 2
+
+
+// function myFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("myBtn");
+  
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
+
+
+// chapter 53 - 57
+
+function hone(){
+    var i = document.getElementById("imgone");
+    i.style.width="500px"
+    i.style.position="centre"
+}
+function htwo(){
+    var i = document.getElementById("imgtwo");
+    i.style.width="500px"
+    i.style.position="centre"
+}
+function hthree(){
+    var i = document.getElementById("imgthree");
+    i.style.width="500px"
+    i.style.position="centre"
+}
+function hfor(){
+    var i = document.getElementById("imgfor");
+    i.style.width="500px"
+    i.style.position="centre"
+}
